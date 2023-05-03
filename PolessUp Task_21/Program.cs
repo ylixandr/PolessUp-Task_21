ad95fdb7d84426d0a22214be270034265ca45dad
Ylixandr <savoshinsky@inbox.ru>
2023-05-03 12:54:39 +0300

    Добавьте файлы проекта.

---
+++ PolessUp Task_21/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+namespace PolessUp_Task_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            Rectangle1 rect1 = new Rectangle1(-3, 0, 3, 4);
+            
+           
+            Rectangle1 rect2 = new Rectangle1(0, -1, 9, 2);
+            Rectangle1[] rectangle1s = {rect1, rect2};
+            
+            if ((rect1.X1 < rect2.X1 && rect1.Y1 < rect2.Y1 && rect1.X2 < rect2.X2 && rect1.Y2 < rect2.Y2) || (rect2.X1 < rect1.X1 && rect2.Y1 < rect2.Y1 && rect2.X2 < rect1.X2 && rect2.Y2 < rect2.Y2))
+            {
+                var res = rectangle1s.Sum(a => a.Area);
+                Console.WriteLine(res);
+            }
+            else
+            {
+                Console.WriteLine("Прямоугольники пересекаются");
+
+            }
+
+
+        }
+    }
+}
---
+++ PolessUp Task_21/Rectangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolessUp_Task_21
+{
+    internal class Rectangle1
+    {
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+
+        public Rectangle1(int x1 , int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+           
+        }
+
+        public int X1
+        {
+            get { return x1; }
+        }
+
+        public int X2
+        {
+            get { return x2; }
+        }
+
+        public int Y1
+        { get { return y1; } }
+
+        public int Y2
+        { get { return y2; } }
+
+        public double Area
+        {
+           
+            get { return (x2-x1)*(y2-y1);}
+        }
+
+
+    }
+}
